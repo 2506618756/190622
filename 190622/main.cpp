a726d24c1f1daedaa8b990dbe70c9c1eb1a606da
晨曦 <2506618756@qq.com>
2019-06-22 14:46:44 +0800

    Initial Commit

---
+++ 190622/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  190622
+//
+//  Created by S20181105302 on 2019/6/22.
+//  Copyright © 2019 S20181105302. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
